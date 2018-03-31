1e3b8288a8d5c231bae43886229c170f01b8be92
Jeff Kubascik <jeffkub@gmail.com>
2018-03-31 11:20:52 -0400

    Cleaning up logger script

--- firmware/mqtt-logger/logger.py
+++ firmware/mqtt-logger/logger.py
@@ -1,85 +1,82 @@
 #!/usr/bin/python3
 
+import argparse
 import json
 import re
 import sqlite3
 
+import daemon
 import paho.mqtt.client as mqtt
 
-DB_FILE = 'data.db'
-
-MQTT_SERVER = 'mqttpi.rawr.lan'
-MQTT_PORT = 1883
 MQTT_TOPIC_BASE = 'brewing/hydrometer/'
 
 hydro_json_re = re.compile(MQTT_TOPIC_BASE + '(.*)/json')
 
 
-def init_db():
-    conn = sqlite3.connect(DB_FILE)
+class Database:
+    def __init__(self, path):
+        self.path = path
+        self.setup()
 
-    c = conn.cursor()
-    c.execute('''
-        CREATE TABLE IF NOT EXISTS devices(
-            id INTEGER PRIMARY KEY,
-            name TEXT UNIQUE,
-            cur_session INTEGER DEFAULT 0
-        )
-    ''')
+    def setup(self):
+        conn = sqlite3.connect(self.path)
+        with conn:
+            conn.execute('''
+                CREATE TABLE IF NOT EXISTS devices(
+                    id INTEGER PRIMARY KEY,
+                    name TEXT UNIQUE,
+                    cur_session INTEGER DEFAULT 0
+                )
+            ''')
 
-    c.execute('''
-        CREATE TABLE IF NOT EXISTS log(
-            id INTEGER PRIMARY KEY,
-            device INTEGER,
-            session INTEGER,
-            bat_v REAL,
-            temp_f REAL,
-            light_vis REAL,
-            light_ir REAL,
-            light_uv REAL,
-            accel_x REAL,
-            accel_y REAL,
-            accel_z REAL,
-            tilt REAL,
-            wifi_conn_fail INTEGER,
-            mqtt_conn_fail INTEGER,
-            last_run_time REAL,
-            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY(device) REFERENCES devices(id)
-        )
-    ''')
+            conn.execute('''
+                CREATE TABLE IF NOT EXISTS log(
+                    id INTEGER PRIMARY KEY,
+                    device INTEGER,
+                    session INTEGER,
+                    bat_v REAL,
+                    temp_f REAL,
+                    light_vis REAL,
+                    light_ir REAL,
+                    light_uv REAL,
+                    accel_x REAL,
+                    accel_y REAL,
+                    accel_z REAL,
+                    tilt REAL,
+                    wifi_conn_fail INTEGER,
+                    mqtt_conn_fail INTEGER,
+                    last_run_time REAL,
+                    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+                    FOREIGN KEY(device) REFERENCES devices(id)
+                )
+            ''')
 
-    conn.commit()
-    conn.close()
+        conn.close()
 
+    def insert_log(self, device, data):
+        conn = sqlite3.connect(self.path)
+        with conn:
+            conn.execute('''
+                INSERT OR IGNORE INTO devices( name ) VALUES( :device )
+            ''', {'device': device})
 
-def log_data(device, data):
-    conn = sqlite3.connect(DB_FILE)
+            conn.execute('''
+                INSERT INTO log(
+                    device, session,
+                    bat_v, temp_f,
+                    light_vis, light_ir, light_uv,
+                    accel_x, accel_y, accel_z, tilt,
+                    wifi_conn_fail, mqtt_conn_fail, last_run_time )
+                SELECT
+                    id, cur_session,
+                    :bat_v, :temp_f,
+                    :light_vis, :light_ir, :light_uv,
+                    :accel_x, :accel_y, :accel_z, :tilt,
+                    :wifi_conn_fail, :mqtt_conn_fail, :last_run_time
+                FROM devices WHERE name=:device
+            ''', {'device': device, **data})
 
-    c = conn.cursor()
-
-    c.execute('''
-        INSERT OR IGNORE INTO devices( name ) VALUES( :device )
-    ''', {'device': device})
-
-    c.execute('''
-        INSERT INTO log(
-            device, session,
-            bat_v, temp_f,
-            light_vis, light_ir, light_uv,
-            accel_x, accel_y, accel_z, tilt,
-            wifi_conn_fail, mqtt_conn_fail, last_run_time )
-        SELECT
-            id, cur_session,
-            :bat_v, :temp_f,
-            :light_vis, :light_ir, :light_uv,
-            :accel_x, :accel_y, :accel_z, :tilt,
-            :wifi_conn_fail, :mqtt_conn_fail, :last_run_time
-        FROM devices WHERE name=:device
-    ''', {'device': device, **data})
-
-    conn.commit()
-    conn.close()
+        conn.close()
 
 
 def on_message(client, userdata, msg):
@@ -89,20 +86,40 @@ def on_message(client, userdata, msg):
     if match:
         device = match.group(1)
         payload = json.loads(msg.payload.decode())
-        log_data(device, payload)
+        userdata.insert_log(device, payload)
 
 
-def main():
-    init_db()
+def logger(args):
+    db = Database(args.db)
 
-    client = mqtt.Client()
+    client = mqtt.Client(userdata=db)
     client.on_message = on_message
 
-    client.connect(MQTT_SERVER, MQTT_PORT, 60)
+    client.connect(args.mqtt_host, args.mqtt_port, 60)
     client.subscribe(MQTT_TOPIC_BASE + '+/json', 2)
 
     client.loop_forever()
 
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--daemon', action='store_true',
+                        help='run as a daemon')
+    parser.add_argument('--db', default='data.db',
+                        help='database file')
+    parser.add_argument('--mqtt_host', default='localhost',
+                        help='hostname or IP address of the remote broker')
+    parser.add_argument('--mqtt_port', type=int, default=1883,
+                        help='network port of the remote broker')
+    args = parser.parse_args()
+
+    if(args.daemon):
+        # Start as a daemon
+        with daemon.DaemonContext():
+            logger(args)
+    else:
+        logger(args)
+
+
 if __name__ == "__main__":
-    main()
+        main()
