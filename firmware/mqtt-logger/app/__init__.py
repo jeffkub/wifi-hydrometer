1dd37bab439da0b839309542d38bd52578a83894
Jeff Kubascik <jeffkub@gmail.com>
2018-04-02 20:58:47 -0400

    Implemented mqtt subscription

--- firmware/mqtt-logger/app/__init__.py
+++ firmware/mqtt-logger/app/__init__.py
@@ -2,10 +2,24 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mqtt import Mqtt
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mqtt = Mqtt(app)
+
 
 from app import routes, models
+
+
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    mqtt.subscribe('brewing/hydrometer/+/json')
+
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    print(message.topic + ' ' + message.payload.decode())
--- firmware/mqtt-logger/config.py
+++ firmware/mqtt-logger/config.py
@@ -7,3 +7,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MQTT_BROKER_URL = 'localhost'
+    MQTT_BROKER_PORT = 1883
+    MQTT_USERNAME = 'user'
+    MQTT_PASSWORD = 'pass'
+    MQTT_REFRESH_TIME = 1.0
