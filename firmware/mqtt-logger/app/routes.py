e1a88cf586784e9e058099c58b58bfcaf087bbf7
Jeff Kubascik <jeffkub@gmail.com>
2018-04-02 20:45:05 -0400

    Showing all log entries for a device

--- firmware/mqtt-logger/app/routes.py
+++ firmware/mqtt-logger/app/routes.py
@@ -1,9 +1,11 @@
 from flask import render_template
 from app import app
+from app.models import Device
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Test'}
-    return render_template('index.html', user=user)
+    device = Device.query.filter_by(name='test').first_or_404()
+    logs = device.logs.all()
+    return render_template('index.html', device=device, logs=logs)
